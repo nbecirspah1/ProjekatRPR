ee9c142406bcbe2ecb0cbe0028d32d8281f5bb46
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-05 07:45:49 +0100

    Dodana klasa Korpa sa implementiranim konstruktorom, setterima i getterima

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Korpa.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr.domain;
+
+public class Korpa implements Idable{
+    private int id;
+    private int proizvodID;
+    private int totalCost;
+
+    public Korpa(int proizvodID, int totalCost) {
+        this.proizvodID = proizvodID;
+        this.totalCost = totalCost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProizvodID() {
+        return proizvodID;
+    }
+
+    public void setProizvodID(int proizvodID) {
+        this.proizvodID = proizvodID;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(int totalCost) {
+        this.totalCost = totalCost;
+    }
+}
