66dd5d993ff1690cc17c5d230b6c0da25d7ec2ce
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-05 17:06:33 +0100

    Implementiran interfejs PlacanjeDao i klasa PlacanjeDaoSQLImpl

---
+++ src/main/java/ba/unsa/etf/rpr/dao/PlacanjeDao.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Placanje;
+
+public interface PlacanjeDao extends Dao<Placanje>{
+
+}
---
+++ src/main/java/ba/unsa/etf/rpr/dao/PlacanjeDaoSQLImpl.java
@@ -0,0 +1,42 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Placanje;
+import ba.unsa.etf.rpr.exceptions.ProjekatException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class PlacanjeDaoSQLImpl extends AbstractDao<Placanje> implements PlacanjeDao{
+
+
+    public PlacanjeDaoSQLImpl() {
+        super("Placanje");
+    }
+
+    @Override
+    public Placanje row2object(ResultSet rs) throws ProjekatException{
+        try {
+            Placanje placanje = new Placanje();
+            placanje.setId(rs.getInt("idPlacanje"));
+            placanje.setKorpaID(rs.getInt("korpaID"));
+            placanje.setPaymentDate(rs.getDate("DatumPlacanja"));
+            placanje.setPaymentMethod(rs.getString("NacinPlacanja"));
+            return placanje;
+        }catch(SQLException e){
+            throw  new ProjekatException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row (Placanje object){
+        Map<String, Object> row = new TreeMap<>();
+        row.put("idPlacanje", object.getId());
+        row.put("korpaID", object.getKorpaID());
+        row.put("DatumPlacanja", object.getPaymentDate());
+        row.put("NacinPlacanja", object.getPaymentMethod());
+        return row;
+    }
+
+}
