f71e1317b05180d60e754634daa1ad7cb240c9e5
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-17 18:43:09 +0100

    Uredjen welcome.css i u start metodi postavljeno da se prvo otvara welcome przor, također dodana ikona aplikacije

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,11 +2,15 @@ package ba.unsa.etf.rpr;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+import static javafx.scene.control.PopupControl.USE_PREF_SIZE;
 
 /**
  * Hello world!
@@ -21,10 +25,13 @@ public class App extends Application
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/signup.fxml"));
-        primaryStage.setTitle("Hello, world!");
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/welcome.fxml"));
+        primaryStage.setTitle("Welcome!");
+        Image icon = new Image(getClass().getResourceAsStream("/img/logo.jpg"));
+        primaryStage.getIcons().add(icon);
+      //  Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
         primaryStage.show();
     }
 }
