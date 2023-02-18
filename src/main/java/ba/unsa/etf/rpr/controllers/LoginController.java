dafffd367ebf51da71b8aeb51416303078363c01
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-18 04:36:32 +0100

    Prepravljeno da se novi scene otvara na istom Stageu

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -4,6 +4,7 @@ import ba.unsa.etf.rpr.business.KupacManager;
 import ba.unsa.etf.rpr.exceptions.ProjekatException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -37,14 +38,14 @@ public class LoginController {
         }
         else{
             Parent newRoot = FXMLLoader.load(getClass().getResource("/fxml/shop.fxml"));
-            Stage stage = new Stage();
+            Node node = (Node) actionEvent.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
             stage.setTitle("Online shop");
             Image icon = new Image(getClass().getResourceAsStream("/img/logo.jpg"));
             stage.getIcons().add(icon);
-            Scene scene = new Scene(newRoot, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
+            Scene currentScene = stage.getScene();
+            currentScene.setRoot(newRoot);
+
         }
     }
 }
