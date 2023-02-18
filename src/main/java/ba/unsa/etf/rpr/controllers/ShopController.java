530675911c8ec15a5a7b8a3a5d3768a3b6b74eca
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-18 20:07:18 +0100

    Dodan prikaz opisa proizvoda i cijene proizvoda

--- src/main/java/ba/unsa/etf/rpr/controllers/ShopController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ShopController.java
@@ -4,16 +4,20 @@ import ba.unsa.etf.rpr.business.ProizvodManager;
 import ba.unsa.etf.rpr.domain.Proizvod;
 import ba.unsa.etf.rpr.exceptions.ProjekatException;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 
 
 import java.io.*;
 import java.util.List;
 
+import static com.sun.tools.javac.tree.JCTree.Tag.POS;
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class ShopController {
@@ -32,7 +36,17 @@ public class ShopController {
                    // System.out.println("OVO JE KAO KAO KAO SLIKA " + imageView);
                     imageView.setFitWidth(400);
                     imageView.setFitHeight(400);
-                    flowPaneID.getChildren().add(imageView);
+
+                    Label opis = new Label(proizvod.getOpis());
+                    opis.setAlignment(Pos.CENTER);
+                    Label cijena = new Label(String.format("%.2f KM", proizvod.getCijena()));
+                    cijena.setAlignment(Pos.CENTER);
+
+                    VBox vBox = new VBox(imageView, opis, cijena);
+                    vBox.setAlignment(Pos.CENTER);
+                    vBox.setSpacing(10.0);
+
+                    flowPaneID.getChildren().add(vBox);
                 }catch(Exception e){
                     new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
 
