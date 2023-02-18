549a15d19ce59acbc9ed2ac1975903cddefa32ce
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-18 04:31:42 +0100

    Implementirana metoda onButtonClick za klasu LoginController, te dodan fxml fajl u kojem ce biti implementiran izgled glavne stranice projekta

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,9 +1,21 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.KupacManager;
+import ba.unsa.etf.rpr.exceptions.ProjekatException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class LoginController {
 
@@ -15,9 +27,24 @@ public class LoginController {
 
 
 
-    public void onButtonClick(ActionEvent actionEvent) {
-        String ime = emailID.getText();
+    public void onButtonClick(ActionEvent actionEvent) throws ProjekatException, IOException {
+        String email = emailID.getText();
         String password = passwordID.getText();
 
+        if(manager.searchByEmail(email).isEmpty()){
+            new Alert(Alert.AlertType.NONE, "Ne postoji account sa ovim emailom!", ButtonType.OK).show();
+
+        }
+        else{
+            Parent newRoot = FXMLLoader.load(getClass().getResource("/fxml/shop.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Online shop");
+            Image icon = new Image(getClass().getResourceAsStream("/img/logo.jpg"));
+            stage.getIcons().add(icon);
+            Scene scene = new Scene(newRoot, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        }
     }
 }
