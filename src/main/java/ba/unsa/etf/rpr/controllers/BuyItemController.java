8320077b1ae1192bf740e964967242deab5b0f24
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-19 18:51:40 +0100

    Obrisani redudantni importi

--- src/main/java/ba/unsa/etf/rpr/controllers/BuyItemController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/BuyItemController.java
@@ -1,14 +1,11 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.Proizvod;
-import ba.unsa.etf.rpr.exceptions.ProjekatException;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
