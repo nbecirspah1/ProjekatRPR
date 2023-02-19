42832719aef392bf54ad5c4045e396662d6d0d4f
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-19 22:30:08 +0100

    Dodan label za error ukoliko kupac nije odabrao nacin kupovine i modifikovana metoda onKupiClicked

--- src/main/java/ba/unsa/etf/rpr/controllers/BuyItemController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/BuyItemController.java
@@ -11,10 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -39,10 +36,14 @@ public class BuyItemController {
     public Label iznosLabel;
     public CheckBox checkBoxGotovina;
     public CheckBox checkBoxKartica;
+    public Button buyButton;
+    public Label errorLabel;
     KupacManager manager = new KupacManager();
     @FXML
     public void initialize() throws ProjekatException {
+        errorLabel.setText("");
         int id = SessionManager.getInstance().getKupacId();
+
         System.out.println("Ovo je id:" + id);
         System.out.println("Ovo je proizvod" + SessionManager.getInstance().getProizvod().getOpis());
         try{
@@ -79,23 +80,34 @@ public class BuyItemController {
         if (checkBoxGotovina.isSelected()) {
             checkBoxGotovina.setSelected(false);
         }
+        errorLabel.setText("");
     }
 
     public void onGotovinaClicked(ActionEvent actionEvent) {
         if (checkBoxKartica.isSelected()){
             checkBoxKartica.setSelected(false);
         }
+        errorLabel.setText("");
+
     }
 
     public void onKupiClicked(ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/uspjesnaKupovina.fxml"));
-        Stage stage = new Stage();
-        stage.setTitle("Uspješna kupovina");
-        Image icon = new Image(getClass().getResourceAsStream("/img/logo.jpg"));
-        stage.getIcons().add(icon);
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        stage.show();
+        if(!checkBoxKartica.isSelected() && !checkBoxGotovina.isSelected()){
+            if(errorLabel.getText() == ""){
+                errorLabel.setText("Niste odabrali način kupovine");
+            }
+        }
+        else{
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/uspjesnaKupovina.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Uspješna kupovina");
+            Image icon = new Image(getClass().getResourceAsStream("/img/logo.jpg"));
+            stage.getIcons().add(icon);
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+
+        }
 
 
     }
--- src/main/java/ba/unsa/etf/rpr/controllers/UspjesnaKupovinaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UspjesnaKupovinaController.java
@@ -1,8 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 
 public class UspjesnaKupovinaController {
     public void onOkClick(ActionEvent actionEvent) {
+        Node node = (Node) actionEvent.getSource();
+        Stage stageOld = (Stage) node.getScene().getWindow();
+        stageOld.close();
+
     }
 }
