48b531b84c65528c10acdc10f6127134c2abb14f
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-20 01:36:19 +0100

    Dodano pozivanje metode add iz klase PlacanjeManager u metodi DaClick

--- src/main/java/ba/unsa/etf/rpr/controllers/NotificationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/NotificationController.java
@@ -1,5 +1,9 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.PlacanjeManager;
+import ba.unsa.etf.rpr.business.SessionManager;
+import ba.unsa.etf.rpr.domain.Placanje;
+import ba.unsa.etf.rpr.exceptions.ProjekatException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -9,18 +13,35 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class NotificationController {
-    public void DAClick(ActionEvent actionEvent) throws IOException {
+
+    PlacanjeManager manager = new PlacanjeManager();
+    public void DAClick(ActionEvent actionEvent) throws IOException, ProjekatException {
+        LocalDate datumPlacanja = LocalDate.now();
+        Date date = Date.from(datumPlacanja.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        String nacinPlacanja = SessionManager.getInstance().getNacinPlacanja();
+        int proizvodID = SessionManager.getInstance().getProizvod().getId();
+        int kupacID = SessionManager.getInstance().getKupacId();
+        Placanje placanje = new Placanje();
+        placanje.setPaymentDate(date);
+        placanje.setPaymentMethod(nacinPlacanja);
+        placanje.setProizvodID(proizvodID);
+        placanje.setKupacID(kupacID);
+        manager.add(placanje);
+
         Node node = (Node) actionEvent.getSource();
         Stage stageOld = (Stage) node.getScene().getWindow();
         stageOld.close();
 
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/uspjesnaKupovina.fxml"));
         Stage stage = new Stage();
-        stage.setTitle("Uspješna kupovina");
+        stage.setTitle("Pitanje");
         Image icon = new Image(getClass().getResourceAsStream("/img/logo.jpg"));
         stage.getIcons().add(icon);
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
