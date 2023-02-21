0d1e59a14ef854cd6216c9e2b1214c12a38a8aa6
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-21 06:24:09 +0100

    Izmijenjen naziv klase App u AppFX dodana klasa App u kojoj cemo implementirati CLI

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -2,21 +2,18 @@ package ba.unsa.etf.rpr;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
-import static javafx.scene.control.PopupControl.USE_PREF_SIZE;
 
 /**
  * Hello world!
  *
  */
-public class App extends Application
+public class AppFX extends Application
 {
     public static void main( String[] args )
     {
