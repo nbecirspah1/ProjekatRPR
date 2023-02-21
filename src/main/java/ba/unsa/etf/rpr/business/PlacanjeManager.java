2af7fae490581c780e9cacd003a393c5039378e2
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-21 11:10:58 +0100

    JavaDoc komentar

--- src/main/java/ba/unsa/etf/rpr/business/PlacanjeManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/PlacanjeManager.java
@@ -12,7 +12,12 @@ import java.util.List;
 public class PlacanjeManager {
 
 
-
+    /**
+     * Dodavanje podataka u tabelu Placanje
+     * @param placanje
+     * @return
+     * @throws ProjekatException
+     */
     public Placanje add(Placanje placanje) throws ProjekatException {
         if(placanje.getId() != 0){
             throw new ProjekatException("Kategorija sa postojećim ID-em se ne može dodati. ID mora biti automatski generisan.");
