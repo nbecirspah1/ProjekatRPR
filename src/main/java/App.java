6cc864f91889e65eb4c6f095617bd75bcedf5db0
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2023-02-21 06:31:16 +0100

    Definisane varijable tipa Option

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,25 @@
+
+import net.bytebuddy.asm.Advice;
+import org.apache.commons.cli.*;
+import java.sql.Date;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Stream;
+
+
+
 public class App {
+    /**
+     * Definisanje varijabli za opcije
+     */
+    private static final Option addUser = new Option("u", "add-user", false, "Adding new user to RPRProjekaBaza database\"");
+    private static final Option getItems = new Option("getI", "get-items", false, "Printing all items from RPRProjekaBaza database\"");
+    private static final Option getCategories = new Option("getC", "get-categories",false, "Printing all categories from RPRProjekaBaza database");
+
+
 
 }
